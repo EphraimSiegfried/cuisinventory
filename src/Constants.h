d90002470a43e981207d5a8375ede5bbe7856730
kev1nbam27 <kevin1bam@web.de>
2023-12-27 15:59:05 +0100

    add setup stuff to main.ino and add some constants

--- src/Constants.h
+++ src/Constants.h
@@ -1,4 +1,13 @@
 #pragma once
 
-const String MAPPINGFILE = "/.intern/mapping";
-const String STATEFILE = "/.intern/state";
+// SD
+const int SD_PIN = 10;
+const String MAPPINGFILE = "/.state/mapping";
+const String STATEFILE = "/.state/state";
+
+// Buttons
+const uint8_t BTN_LIGHT_IDLE = 10; // brightness of buttons while idle, between 0 and 255
+const uint8_t BTN_LIGHT_ACTIVE = 50; // brightness of buttons during click, between 0 and 255
+
+// LCD
+const uint8_t LCD_CONTRAST = 5;
--- src/Main.ino
+++ src/Main.ino
@@ -1,28 +1,167 @@
 #include "DB.h"
 #include <SD.h>
-#include <SPI.h>
-#include <WiFiNINA.h>
+#include <Wire.h>
+#include <SerLCD.h>
+#include <RTClib.h>
+#include "Constants.h"
 #include <ArduinoJson.h>
+#include <Adafruit_TinyUSB.h>
+#include <SparkFun_Qwiic_Button.h>
 
-  //Initialize serial and wait for port to open:
+#define _TASK_STATUS_REQUEST     // Compile with support for StatusRequest functionality - triggering tasks on status change events in addition to time only
+// #define _TASK_LTS_POINTER        // Compile with support for local task storage pointer
+#include <TaskScheduler.h>
 
-  void setup() {
+Scheduler ts;
+
+enum buttons {
+    GREEN_BUTTON1 = 0x6f,
+    GREEN_BUTTON2 = 0x70,
+    RED_BUTTON = 0x71
+};
+
+SerLCD lcd; 
+Sd2Card card;
+SdVolume volume;
+RTC_PCF8523 rtc;
+QwiicButton greenButton1;
+QwiicButton greenButton2;
+QwiicButton redButton;
+Adafruit_USBD_MSC usb_msc;
+
+bool usb = false; // whether to act as usb mass storage
+
+void setup() {
+#ifdef DEBUG
     Serial.begin(9600);
-    while (!Serial) {
-      ; // wait for serial port to connect. Needed for native USB port only
-    }
-    SD.begin(10);
-    if(!initializeSD()){
-      Serial.println("SD Card initialization failed");
-    }
-    if(!connectWlan("S10","Internet5")){
-      Serial.println("wlan connection failed");
-    }
-    Serial.println("wlan connection successfull");
-    fetchData("world.openfoodfacts.org","/api/v3/product/","3017620422003","product_name,generic_name,allergens,conservation_conditions,nutriscore_grade,ingredients_text,customer_service");
-  
-  }
+#endif
+    Wire.begin();
+    lcd.begin(Wire);
+    Wire.setClock(400000); // set I2C SCL to High Speed Mode of 400kHz
 
-  void loop() {
-  
-  }
+    lcd.setFastBacklight(0xFFFFFF); // set backlight to bright white
+    lcd.setContrast(LCD_CONTRAST); // Set contrast. Lower to 0 for higher contrast.
+    lcd.clear(); 
+
+    // *** Buttons ***
+
+    while (!greenButton1.begin(GREEN_BUTTON1)) {
+        lcd.clear();
+        lcd.print("Green button 1 not found...");
+        delay(200);
+    }
+    while (!greenButton2.begin(GREEN_BUTTON2)) {
+        lcd.clear();
+        lcd.print("Green button 2 not found...");
+        delay(200);
+    }
+    while (!redButton.begin(RED_BUTTON)) {
+        lcd.clear();
+        lcd.print("Red button not found...");
+        delay(200);
+    }
+
+    greenButton1.LEDon(BTN_LIGHT_IDLE);
+    greenButton2.LEDon(BTN_LIGHT_IDLE);
+    redButton.LEDon(BTN_LIGHT_IDLE);
+
+    lcd.clear();
+
+    // *** USB ***
+
+    // hold red button while plugging in to enable usb msc
+    if (redButton.isPressed()) {
+        lcd.print("** USB MSC activated **");
+        usb = true;
+        setupUSB();
+        return;
+    }
+
+    // *** SD ***
+
+    pinMode(SD_PIN, OUTPUT); // set SD pin mode
+
+    while (!SD.begin(SD_PIN)) {
+        lcd.clear();
+        lcd.print("Please insert SD card...");
+        delay(200);
+    }
+
+    lcd.clear();
+
+    // *** Clock ***
+
+    while (!rtc.begin()) {
+        lcd.clear();
+        lcd.print("Failed to find RTC...");
+        delay(200);
+    }
+
+    if (!rtc.initialized() || rtc.lostPower()) {
+        lcd.clear();
+        lcd.print("RTC is not initialized!");
+        while (1) delay(10);
+    }
+
+    rtc.start();
+    lcd.clear();
+}
+
+void loop() {
+    if (usb) return; // do nothing if in usb mode
+}
+
+void setupUSB() {
+    // Set disk vendor id, product id and revision with string up to 8, 16, 4 characters respectively
+    usb_msc.setID("Vendor", "Cuisinventory", "1.0");
+
+    // Set read write callback
+    usb_msc.setReadWriteCallback(msc_read_cb, msc_write_cb, msc_flush_cb);
+
+    // Still initialize MSC but tell usb stack that MSC is not ready to read/write
+    // If we don't initialize, board will be enumerated as CDC only
+    usb_msc.setUnitReady(false);
+    usb_msc.begin();
+
+    if (!card.init(SPI_HALF_SPEED, SD_PIN)) {
+        lcd.clear();
+        lcd.print("Failed to initialize SD!");
+        while (1) delay(10);
+    }
+
+    // Now we will try to open the 'volume'/'partition' - it should be FAT16 or FAT32
+    if (!volume.init(card)) {
+        lcd.clear();
+        lcd.print("Couldn't find FAT16/FAT32 partition!");
+        while (1) delay(10);
+    }
+
+    uint32_t block_count = volume.blocksPerCluster()*volume.clusterCount();
+    usb_msc.setCapacity(block_count, 512); // Set disk size, SD block size is always 512
+    usb_msc.setUnitReady(true); // msc is ready for read/write
+}
+
+// Callback invoked when received READ10 command.
+// Copy disk's data to buffer (up to bufsize) and
+// return number of copied bytes (must be multiple of block size)
+int32_t msc_read_cb (uint32_t lba, void* buffer, uint32_t bufsize)
+{
+  (void) bufsize;
+  return card.readBlock(lba, (uint8_t*) buffer) ? 512 : -1;
+}
+
+// Callback invoked when received WRITE10 command.
+// Process data in buffer to disk's storage and 
+// return number of written bytes (must be multiple of block size)
+int32_t msc_write_cb (uint32_t lba, uint8_t* buffer, uint32_t bufsize)
+{
+  (void) bufsize;
+  return card.writeBlock(lba, buffer) ? 512 : -1;
+}
+
+// Callback invoked when WRITE10 command is completed (status received and accepted by host).
+// used to flush any pending cache.
+void msc_flush_cb (void)
+{
+  // nothing to do
+}
