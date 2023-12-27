f465297518111c7f2f0d47da91bfb0cb55757091
Ephraim Siegfried <e.siegfried@stud.unibas.ch>
2023-12-27 16:30:22 +0100

    Add wifi constants

--- src/Constants.h
+++ src/Constants.h
@@ -2,3 +2,10 @@
 
 const String MAPPINGFILE = "/.intern/mapping";
 const String STATEFILE = "/.intern/state";
+
+const String BARCODE_ENDPOINT = "world.openfoodfacts.org";
+const String BARCODE_PATH = "/api/v3/product/";
+const String BARCODE_FIELDS =
+    "product_name,generic_name,allergens,conservation_conditions,nutriscore_"
+    "grade,ingredients_text,customer_service";
+const String USER_AGENT = "Cuisinventory/1.0 alexander.lutsch@stud.unibas.ch";
