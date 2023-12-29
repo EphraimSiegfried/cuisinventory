3875049e50c57ed3d71713013275095ae75f3f78
kev1nbam27 <kevin1bam@web.de>
2023-12-29 18:26:01 +0100

    add input function

---
+++ include/Input.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <Constants.h>
+#include <SparkFun_Qwiic_Button.h>
+
+extern QwiicButton greenButton1;
+extern QwiicButton greenButton2;
+extern QwiicButton redButton;
+
+typedef enum Button { GREEN_BUTTON1 = 0, GREEN_BUTTON2, RED_BUTTON } Button;
+typedef enum Press { SHORT_PRESS = 0, LONG_PRESS } Press;
+
+
+bool input(Button btn, Press press);
