42c6e5757d9a27705da1ac66297974d8c1593cc8
Lint Action <lint-action@samuelmeuli.com>
2023-12-29 13:48:28 +0000

    Fix code style issues with clang_format

--- include/USB.h
+++ include/USB.h
@@ -9,4 +9,4 @@
 void setupUSB();
 int32_t msc_read_cb(uint32_t lba, void* buffer, uint32_t bufsize);
 int32_t msc_write_cb(uint32_t lba, uint8_t* buffer, uint32_t bufsize);
-void msc_flush_cb (void);
+void msc_flush_cb(void);
