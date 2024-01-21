8dc853e3ea83e0944ea0a048b1a4c5979505fca9
kev1nbam27 <kevin1bam@web.de>
2024-01-21 15:13:06 +0100

    fix USB lib

--- lib/USB/USB.cpp
+++ lib/USB/USB.cpp
@@ -1,11 +1,18 @@
 #include <USB.h>
 
 SdFat card;
+
+SdFile root;
+SdFile file;
+
 Adafruit_USBD_MSC usb_msc;
+
+bool fs_changed;
 
 void setupUSB() {
     // Set disk vendor id, product id and revision with string up to 8, 16, 4
-    // characters respectively usb_msc.setID("Vendor", "Cuisinventory", "1.0");
+    // characters respectively
+    usb_msc.setID("Vendor", "Cuisinventory", "1.0");
 
     // Set read write callback
     usb_msc.setReadWriteCallback(msc_read_cb, msc_write_cb, msc_flush_cb);
@@ -20,15 +27,14 @@ void setupUSB() {
         while (1) delay(10);
     }
 
-    // Now we will try to open the 'volume'/'partition' - it should be FAT16 or
-    // FAT32
-    if (!card.vol()->init(card.card())) {
-        LOG("Couldn't find FAT16/FAT32 partition!");
-        while (1) delay(10);
-    }
+    // Size in blocks (512 bytes)
+#if SD_FAT_VERSION >= 20000
+    uint32_t block_count = card.card()->sectorCount();
+#else
+    uint32_t block_count = card.card()->cardSize();
+#endif
 
-    uint32_t block_count =
-        card.vol()->sectorsPerCluster() * card.vol()->clusterCount();
+    uint32_t block_count = card.card()->sectorCount();
     usb_msc.setCapacity(block_count,
                         512);    // Set disk size, SD block size is always 512
     usb_msc.setUnitReady(true);  // msc is ready for read/write
@@ -37,22 +43,45 @@ void setupUSB() {
 // Callback invoked when received READ10 command.
 // Copy disk's data to buffer (up to bufsize) and
 // return number of copied bytes (must be multiple of block size)
-int32_t msc_read_cb(uint32_t lba, void* buffer, uint32_t bufsize) {
-    (void)bufsize;
-    return card.card()->readBlock(lba, (uint8_t*)buffer) ? 512 : -1;
+int32_t msc_read_cb (uint32_t lba, void* buffer, uint32_t bufsize)
+{
+    bool rc;
+
+#if SD_FAT_VERSION >= 20000
+    rc = card.card()->readSectors(lba, (uint8_t*) buffer, bufsize/512);
+#else
+    rc = card.card()->readBlocks(lba, (uint8_t*) buffer, bufsize/512);
+#endif
+
+    return rc ? bufsize : -1;
 }
 
 // Callback invoked when received WRITE10 command.
-// Process data in buffer to disk's storage and
+// Process data in buffer to disk's storage and 
 // return number of written bytes (must be multiple of block size)
-int32_t msc_write_cb(uint32_t lba, uint8_t* buffer, uint32_t bufsize) {
-    (void)bufsize;
-    return card.card()->writeBlock(lba, buffer) ? 512 : -1;
+int32_t msc_write_cb (uint32_t lba, uint8_t* buffer, uint32_t bufsize)
+{
+    bool rc;
+
+#if SD_FAT_VERSION >= 20000
+    rc = card.card()->writeSectors(lba, buffer, bufsize/512);
+#else
+    rc = card.card()->writeBlocks(lba, buffer, bufsize/512);
+#endif
+
+    return rc ? bufsize : -1;
 }
 
-// Callback invoked when WRITE10 command is completed (status received and
-// accepted by host).
+// Callback invoked when WRITE10 command is completed (status received and accepted by host).
 // used to flush any pending cache.
-void msc_flush_cb(void) {
-    // nothing to do
+void msc_flush_cb (void)
+{
+#if SD_FAT_VERSION >= 20000
+    card.card()->syncDevice();
+#else
+    card.card()->syncBlocks();
+#endif
+
+    // clear file system's cache to force refresh
+    card.cacheClear();
 }
